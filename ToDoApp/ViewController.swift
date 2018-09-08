92e918678c7416f121c3fe2e6aa341add85b2abd
daltonbr <daltonvarussa@gmail.com>
2018-09-08 00:32:46 -0300

    finished app

--- ToDoApp/ViewController.swift
+++ ToDoApp/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var txtAddItem: UITextField!
+    @IBOutlet weak var txtList: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func didTapButton(_ sender: Any) {
+        if let text = txtAddItem.text {
+            if text == "" {
+                return
+            }
+            txtList.text.append("\(text)\n")
+            txtAddItem.text = ""
+            txtAddItem.resignFirstResponder()
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
