41a41400a1cf394a6697c18eabbe26aed3d3eedf
daltonbr <daltonvarussa@gmail.com>
2018-09-09 18:18:47 -0300

    populating an UITableView

--- ToDoApp/ViewController.swift
+++ ToDoApp/ViewController.swift
@@ -8,31 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    @IBOutlet weak var txtAddItem: UITextField!
-    @IBOutlet weak var txtList: UITextView!
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var table: UITableView!
+    var data:[String] = ["Item 1", "Item 2", "Item 3"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    @IBAction func didTapButton(_ sender: Any) {
-        if let text = txtAddItem.text {
-            if text == "" {
-                return
-            }
-            txtList.text.append("\(text)\n")
-            txtAddItem.text = ""
-            txtAddItem.resignFirstResponder()
-        }
+        table.dataSource = self
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = UITableViewCell()
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
+    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
