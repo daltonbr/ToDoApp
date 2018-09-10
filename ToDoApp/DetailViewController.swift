5ffe2c37d0312e063dd419e71a19831d42b7f3e3
daltonbr <daltonvarussa@gmail.com>
2018-09-10 17:46:17 -0300

    send data back to the original view controller

--- ToDoApp/DetailViewController.swift
+++ ToDoApp/DetailViewController.swift
@@ -12,6 +12,7 @@ class DetailViewController: UIViewController {
 
     @IBOutlet weak var textView: UITextView!
     var text:String = ""
+    var masterView:ViewController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +26,11 @@ class DetailViewController: UIViewController {
         if isViewLoaded {
             textView.text = t
         }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        masterView.newRowText = textView.text
     }
     
     override func didReceiveMemoryWarning() {
